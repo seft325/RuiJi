6163fc4e80410188b67aab393964a5deb85c4143
seft <1922912874@qq.com>
2024-11-05 19:27:14 +0800

    第一次提交

---
+++ src/main/java/com/seft/ruiji/RuiJi.java
@@ -0,0 +1,15 @@
+package com.seft.ruiji;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@Slf4j
+@SpringBootApplication
+public class RuiJi {
+    public static void main(String[] args) {
+     SpringApplication.run(RuiJi.class, args);
+     log.info("项目启动成功");
+    }
+}
